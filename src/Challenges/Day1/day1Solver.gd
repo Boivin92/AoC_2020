fba4518c5ab1bdbf2de8e24eb617fde0821a32c3
Francois Boivin <boivin.92@gmail.com>
2020-12-01 08:00:26 -0500

    Day 1 - solved
    Could use some cleanup and optimization, tbh

--- src/Challenges/Day1/day1Solver.gd
+++ src/Challenges/Day1/day1Solver.gd
@@ -1,12 +1,38 @@
 extends Solver
 
-func _ready():
+func _init():
 	#FILENAME ONLY INCLUDING EXTENSION
 	#MUST BE IN Folder "res://Challenges/Inputs/"
-	inputFileName = ""; 
+	inputFileName = "Day1.txt"; 
 
 func solve_challenge() -> String:
-	return "un deux un deux patate"
+	var objective := 2020
+	var data : PoolStringArray = load_data()
+	var dataAsNumbers : PoolIntArray
+	for line in data:
+		dataAsNumbers.append(int(line))
+	for i in dataAsNumbers.size():
+		for j in dataAsNumbers.size():
+			if dataAsNumbers[i] == dataAsNumbers[j]:
+				continue
+			else:
+				if dataAsNumbers[i] + dataAsNumbers[j] == objective:
+					return str(dataAsNumbers[i]*dataAsNumbers[j])
+		
+	return ":("
 
 func solve_bonus() -> String:
-	return ""
+	var objective := 2020
+	var data : PoolStringArray = load_data()
+	var dataAsNumbers : PoolIntArray
+	for line in data:
+		dataAsNumbers.append(int(line))
+	for i in dataAsNumbers.size():
+		for j in dataAsNumbers.size():
+			for k in dataAsNumbers.size():
+				if i == j or j == k or i == k:
+					continue
+				else:
+					if dataAsNumbers[i] + dataAsNumbers[j] + dataAsNumbers[k] == objective:
+						return str(dataAsNumbers[i]*dataAsNumbers[j]*dataAsNumbers[k])
+	return ":("
--- src/Challenges/Solver.gd
+++ src/Challenges/Solver.gd
@@ -3,12 +3,12 @@ class_name Solver
 
 var inputFileName
 
-func load():
+func load_data():
 	var file = File.new()
 	file.open("res://Challenges/Inputs/" + inputFileName, File.READ)
 	var content = file.get_as_text()
 	file.close()
-	return content
+	return content.split("\n")
 
 func solve_challenge() -> String:
 	return ""
