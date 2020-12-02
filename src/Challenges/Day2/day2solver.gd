017941584425253670a14a7b8c62a8ea60f43c01
Francois Boivin <boivin.92@gmail.com>
2020-12-02 08:32:56 -0500

    Day2

---
+++ src/Challenges/Day2/day2solver.gd
@@ -0,0 +1,50 @@
+extends Solver
+
+func _init():
+	#FILENAME ONLY INCLUDING EXTENSION
+	#MUST BE IN Folder "res://Challenges/Inputs/"
+	inputFileName = "Day2.txt"
+	bonusSolved = true #set to true once bonus is solved, yo 
+
+func solve_challenge() -> String:
+	var data = load_data()
+	var validNumber : int
+	for line in data:
+		validNumber += is_password_valid_sled(line)
+	return str(validNumber)
+
+func solve_bonus() -> String:
+	var data = load_data()
+	var validNumber : int
+	for line in data:
+		validNumber += is_password_valid_tobbogan(line)
+	return str(validNumber)
+
+func is_password_valid_sled(line: String):
+	var values = dict_that_shit(line)
+	var numberOfTimes : int
+	for letter in values.password:
+		if letter == values.key:
+			numberOfTimes += 1
+	if numberOfTimes < values.low or numberOfTimes > values.high:
+		return 0
+	return 1
+
+func is_password_valid_tobbogan(line: String):
+	var values = dict_that_shit(line)
+	if (values.password[values.low-1] == values.key) != (values.password[values.high-1] == values.key):
+		return 1
+	return 0
+
+func dict_that_shit(line):
+	var tokens = line.split(" ")
+	var minMaxSplit = tokens[0].split("-")
+	tokens[1] = tokens[1].trim_suffix(":")
+	var index1 = int(minMaxSplit[0])
+	var index2 = int(minMaxSplit[1])
+	return {
+		"password": tokens[2],
+		"key":tokens[1],
+		"low": index1,
+		"high": index2
+	}
