037e01f27767491223fe88a9d2996a216b6e36b4
Francois Boivin <boivin.92@gmail.com>
2020-12-10 00:12:51 -0500

    Day10 silver star

--- src/Challenges/Day10/day10Solver.gd
+++ src/Challenges/Day10/day10Solver.gd
@@ -6,7 +6,22 @@ func _ready():
 #	bonusAnswer = ""
 
 func _solve_challenge() -> String:
-	return ""
+	var sortedInputs := []
+	for line in inputData:
+		sortedInputs.append(int(line))
+	sortedInputs.sort()
+	var OJDs := []
+	var TJDs := []
+	if sortedInputs[0] == 1:
+		OJDs.append(0)
+	elif sortedInputs[0] == 3:
+		TJDs.append(0)
+	for i in range(1, sortedInputs.size()):
+		if sortedInputs[i] == (sortedInputs[i-1] + 1):
+			OJDs.append(i)
+		elif sortedInputs[i] == (sortedInputs[i-1] + 3):
+			TJDs.append(i)
+	return str(OJDs.size() * (TJDs.size()+1)) #Count the extra adapter
 
 func _solve_bonus() -> String:
 	return ""
