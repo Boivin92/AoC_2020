f2808bc39d765ef08331a0e542ca001c18e7b698
Francois Boivin <boivin.92@gmail.com>
2020-12-11 23:49:35 -0500

    Day11 gold star

--- src/Challenges/Day11_Seating/SeatLayout.gd
+++ src/Challenges/Day11_Seating/SeatLayout.gd
@@ -2,55 +2,95 @@ extends Node
 class_name SeatLayout
 
 var CurrentLayout := []
+var Y_SIZE := 0
+var X_SIZE := 0
+var occupied_seats := 0
+var cache := {}
 
 func load_layout(inputs):
 	CurrentLayout = inputs
+	Y_SIZE = CurrentLayout.size()
+	X_SIZE = CurrentLayout[0].length()
 
-func calculate_next():
+func calculate_next(lineOfSight = false):
 	var next_layout := []
-	for y in CurrentLayout.size():
+	for y in Y_SIZE:
 		var new_line = ""
-		for x in CurrentLayout[0].length():
-			new_line += process_tile(x,y)
+		for x in X_SIZE:
+			new_line += process_tile(Vector2(x,y), lineOfSight)
 		next_layout.append(new_line)
 	return next_layout
 
 func count_occupied_seats():
 	var total = 0
-	for y in CurrentLayout.size():
-		for x in CurrentLayout[y].length():
-			if is_occupied(x,y):
+	for y in Y_SIZE:
+		for x in X_SIZE:
+			if is_occupied(Vector2(x,y)):
 				total += 1
 	return total
 
-func process_tile(x,y):
-	var pound = "#"
-	var test = CurrentLayout[y][x]
-	match CurrentLayout[y][x]:
-		'.':
+func process_tile(position : Vector2, lineOfSight):
+	match CurrentLayout[position.y][position.x]:
+		'.': #Floor
 			return "."
-		'#':
-			if count_occupied_seats_around(x,y) >= 4:
-				return "L"
-			else:
-				return "#"
-		'L':
-			if count_occupied_seats_around(x,y) == 0:
-				return "#"
-			else:
-				return "L"
+		'#': #Busy Seat
+			var status = status_line_of_sight(position) if lineOfSight else seat_status(position)
+			return "L" if status == "CROWDED" else "#"
+		'L': #Free Seat
+			var status = status_line_of_sight(position) if lineOfSight else seat_status(position)
+			return "#" if status == "QUIET" else "L"
 
-func count_occupied_seats_around(x,y):
-	var adjacents := [[x-1, y-1], [x, y-1], [x+1, y-1],
-	[x-1, y], [x+1, y], [x-1, y+1], [x, y+1], [x+1, y+1]]
+const DIRECTIONS := [
+	Vector2(-1,-1), Vector2(-1,0), Vector2(-1,1),  	#X-1
+	Vector2(0,-1), Vector2(0,1), 					#X
+	Vector2(1,-1), Vector2(1,0), Vector2(1,1)]	#X+1
+
+func seat_status(position : Vector2):
 	var number = 0
-	for a in adjacents:
-		if is_occupied(a[0], a[1]):
+	for d in DIRECTIONS:
+		if is_occupied(position + d):
 			number += 1
-	return number
+		if number == 4:
+			return "CROWDED"
+	return "QUIET" if number == 0 else "OTHER"
 
-func is_occupied(x,y):
-	if x < 0 or y < 0 or y >= CurrentLayout.size() or x >= CurrentLayout[y].length():
+func status_line_of_sight(position : Vector2):
+	var number = 0
+	for d in DIRECTIONS:
+		if busy_seat_in_direction(position, d):
+			number += 1
+			if number == 5:
+				return "CROWDED"
+	return "QUIET" if number == 0 else "OTHER"
+
+func busy_seat_in_direction(position, direction):
+	var posTemp = position + direction
+	if not is_out_of_bounds(posTemp):
+		while is_not_seat(posTemp):
+			posTemp += direction
+			if is_out_of_bounds(posTemp):
+				break
+		if is_occupied(posTemp):
+			return true
+	return false
+
+func is_out_of_bounds(position : Vector2):
+	if 	position.x < 0 or position.y < 0 or \
+		position.y >= Y_SIZE or position.x >= X_SIZE:
+			return true
+	else: 
 		return false
+
+func is_not_seat(position : Vector2):
+	return CurrentLayout[position.y][position.x] == "."
+
+func is_occupied(position : Vector2):
+	if cache.has(position):
+		return cache[position]
+		
+	if 	is_out_of_bounds(position):
+			cache[position] = false
+			return false
 	else:
-		return CurrentLayout[y][x] == "#"
+		cache[position] = CurrentLayout[position.y][position.x] == "#"
+		return CurrentLayout[position.y][position.x] == "#"
--- src/Challenges/Day11_Seating/day11Solver.gd
+++ src/Challenges/Day11_Seating/day11Solver.gd
@@ -1,22 +1,31 @@
 extends Solver
 
+const WITH_LINE_OF_SIGHT = true
+const PROXIMITY = false
+
 func _ready():
-	bonusSolved = false
+	bonusSolved = true
 	dayAnswer = "2448"
-#	bonusAnswer = ""
+	bonusAnswer = "2234"
+
 
 func _solve_challenge() -> String:
-	var states := []
-	states.append(state(Array(inputData)))
-	states.append(state(states[0].calculate_next()))
-	while(states[states.size()-1].CurrentLayout != states[states.size()-2].CurrentLayout):
-		states.append(state(states.back().calculate_next()))
-	return str(states.back().count_occupied_seats())
+	return process_puzzle(PROXIMITY)
+
+func _solve_bonus() -> String:
+	return process_puzzle(WITH_LINE_OF_SIGHT)
+
 
 func state(array):
 	var new_state = SeatLayout.new()
 	new_state.load_layout(array)
 	return new_state
 
-func _solve_bonus() -> String:
-	return ""
+
+func process_puzzle(lineOfSight):
+	var states := []
+	states.append(state(Array(inputData)))
+	states.append(state(states[0].calculate_next(lineOfSight)))
+	while(states[states.size()-1].CurrentLayout != states[states.size()-2].CurrentLayout):
+		states.append(state(states.back().calculate_next(lineOfSight)))
+	return str(states.back().count_occupied_seats())
